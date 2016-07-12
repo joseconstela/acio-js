310e7ddec98492f2e5bcbdbbe2ee5d34814b88ed
Jose Constela <jose@joseconstela.com>
2016-07-12 02:18:36 +0200

    te

--- server.js
+++ server.js
@@ -55,16 +55,17 @@ io.on('connection', (socket) => {
   })
 
   socket.on('result', (result) => {
-
     result.socket = _id
     result.resultId = uuid.v4()
     result.createdAt = new Date()
     result.hashedResult = crypto.createHash('md5').update(JSON.stringify(result.data)).digest('hex')
 
-    mongoDb.collection('JobsResults').insert(result, (err, result) => {
-      if (!err && result) {
+    mongoDb.collection('JobsResults').insert(result, (err, res) => {
+      if (!err && res) {
         if (err) { return false }
-        emitJob(socket)
+        if (!!result.reqNewJob) {
+          emitJob(socket)
+        }
       }
     })
 
