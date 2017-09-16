15603d03deece4134e304773784585eef5326c00
Jose Constela <jose@joseconstela.com>
2017-09-16 18:51:47 +0200

    Improve api

--- routes/api.js
+++ routes/api.js
@@ -8,21 +8,57 @@ module.exports = (dbs) => {
   let api = express.Router();
 
   api.get('/jobs', (req, res) => {
-    res.send('some json');
+
+    let selectFields = {history: false}
+    if (!!req.query.history) { delete selectFields['history'] }
+
+    db.jobs.get(dbs, {
+      query: {},
+      fields: selectFields,
+      transform: ['array']
+    }, (err, result) => {
+      res.send({
+        data: result,
+        totalCount: result.length
+      });
+    })
+      
   });
+
+  api.post('/jobs', (req, res) => {
+
+    let job = _.assign({}, _.pick(req.params, [
+      'name', 'description', 'env', 'template', 'collection'
+    ]))
+
+    db.jobs.insert(job, (err, result) => {
+      res.send(result);
+    })
+
+  })
 
   api.get('/jobs/:jobId', (req, res) => {
-    res.send(req.params.jobId);
-  });
+    
+    let selectFields = {history: false}
+    if (!!req.query.history) { delete selectFields['history'] }
+
+    db.jobs.getOne(dbs, {
+      query: { _id: req.params.jobId },
+      fields: selectFields,
+    }, (err, result) => {
+      res.send(result);
+    })
+
+  })
 
   api.get('/jobs/:jobId/results', (req, res) => {
-    res.send(req.params.jobId);
-  });
+    res.send(req.params.jobId)
+  })
 
   api.get('/jobs/:jobId/results/all', (req, res) => {
-    res.send(req.params.jobId);
-  });
+    res.send(req.params.jobId)
+  })
 
-  return api;
+  return api
 
 }
